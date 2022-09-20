fc975f14d5c5f33f61f8e113c1cf3103a5db1cb3
Emmanuel Omokhegbe <emmanuelozigue@gmail.com>
2022-09-20 18:50:53 +0000

    Python scripts

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,11 @@
+ #!/usr/bin/python3
+
+word = "Holberton"
+
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+
+print(f"First 3 letters: {:2}")
+
+print(f"Last 2 letters: {:-2}")
+
+print(f"Middle word: {:6}")
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+str = "Python is an interpreted, interactive, object-oriented programming\n
+language that combines remarkable power with very clear syntax"
+
+print(str)
