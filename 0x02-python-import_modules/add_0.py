527823edbd72f37dff244fbb3ca734fcf0fb43e8
Emmanuel <emmanuelozigue@gmail.com>
2022-09-22 11:15:12 -0700

    ~

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def add(a, b):
-    print(f"{a} + {b}")
-    return f"{a} + {b} = {a + b}"
+    
+    """My addition function
+    Args:
+    a: first integer
+    b: second integer
+    Return
+    The return value. a + b
+  """
+    return (a + b) 
