31db0c3219916cc46c7693903aabb9d801e1b7b8
Emmanuel <emmanuelozigue@gmail.com>
2022-09-22 05:19:33 -0700

    7-edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,11 +1,15 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 
 word = "Holberton"
 
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+word_first_3 = word[:3]
 
-print(f"First 3 letters: {:2}")
+word_last_2 = word[-2:]
 
-print(f"Last 2 letters: {:-2}")
+middle_word = word[1:-1]
 
-print(f"Middle word: {:6}")
+print("First 3 letters: {}".format(word_first_3))
+
+print("Last 2 letters: {}".format(word_last_2))
+
+print("Middle word: {}".format(middle_word))
