d2954c33d6620464fd7016957a4918cfb9f79290
Thrinkxs <emmanuelozigue@gmail.com>
2023-03-11 04:31:22 -0800

    redid some and fixed bugs

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,38 +1,23 @@
 #include "lists.h"
-/**
- * check_cycle - checks if a singly linked list has a cycle
- * @list: pointer to the head of the list
- * Return: 0 in case of no cycle and 1 at the opposite
- */
 
+/**
+ * check_cycle - Using Floyd's Cycle-Finding Algorithm to check for cycle
+ * in a linked list.
+ * @list: the linked list object
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
-listint_t *p2;
-listint_t *prev;
+	listint_t *slow, *fast;
 
-p2 = list;
-prev = list;
-while (list && p2 && p2 -> next)
-{
-list = list->next;
-p2 = p2->next->next;
-if (list == p2)
-{
-list = prev;
-prev = p2;
-while (1)
-{
-p2 = prev;
-while (p2->next != list && p2 ->next != prev)
-{
-p2 = p2->next;
-}
-if (p2->next == list)
-break;
-list = list->next;
-}
-return (1);
-}
-}
-return(0);
+	slow = fast = list;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
 }
